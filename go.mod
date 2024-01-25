670372ef89b4a24528fb3fafdc15c54a0ff6b051
Charlie Nazario <charlie@nazarios.net>
2024-01-25 11:10:37 -0500

    experimented with route params and added basic middleware

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/charlesnazario/lenslocked
 
 go 1.21.6
 
-require github.com/go-chi/chi/v5 v5.0.11 // indirect
+require github.com/go-chi/chi/v5 v5.0.11
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,11 +31,21 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=uft-8")
+	fmt.Fprint(w, "<h1> you have specified ID "+chi.URLParam(r, "id"))
+}
+
 func main() {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Timeout(60 * time.Second))
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Get("/echo/{id}", echoHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
