58ad9709f4d53b6ebc858cf59925ce3efd9c0a23
Charlie Nazario <charlie@nazarios.net>
2024-01-26 10:12:35 -0500

    added some templates to the project

--- main.go
+++ main.go
@@ -2,33 +2,42 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my AMAZING site!</h1>")
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("Parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, filepath.Join("templates", "home.gohtml"))
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:charlie@gmail.com\">charlie@gmail.com</a>.</p>")
+	executeTemplate(w, filepath.Join("templates", "contact.gohtml"))
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQs</h1>
-		<ul>
-			<li>
-				<b>What is this thing</b>
-				It is not quite known yet, still working on that
-			</li>
-		</ul>
-	`)
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
